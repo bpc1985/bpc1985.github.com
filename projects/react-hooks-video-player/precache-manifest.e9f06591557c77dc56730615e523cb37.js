39ab927a79b0fec6a8a1cb570effd39f0b5cbc45
Hung Ho <hung.ho@vertoanalytics.com>
2018-12-27 16:07:00 +0200

    Add demo project react-hooks-videoplayer

---
+++ projects/react-hooks-video-player/precache-manifest.e9f06591557c77dc56730615e523cb37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "459fc0ccd7042991a48b",
+    "url": "/projects/react-hooks-videoplayer/static/js/runtime~main.459fc0cc.js"
+  },
+  {
+    "revision": "b4c248d5f5711628f8d2",
+    "url": "/projects/react-hooks-videoplayer/static/js/main.b4c248d5.chunk.js"
+  },
+  {
+    "revision": "12901bb93a4d0cb875ec",
+    "url": "/projects/react-hooks-videoplayer/static/js/1.12901bb9.chunk.js"
+  },
+  {
+    "revision": "474584e6a15059c2254d8f61a46538a1",
+    "url": "/projects/react-hooks-videoplayer/index.html"
+  }
+];
